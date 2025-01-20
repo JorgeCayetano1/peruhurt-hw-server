9f98a3664f2e60236ea43d6696dd1cece858a92e
Jorge Cayetano <cayetano.laboral@gmail.com>
2025-01-20 13:25:57 -0500

    add teleportation plugin and delete docker volumes

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,4 +4,4 @@
 cd /home/steam/hurtworld
 
 # Start the Hurtworld server
-./Hurtworld.x86_64 -batchmode -nographics -exec "host 12871;queryport 12881;servername [PERU]HURT;maxplayers 60" -logfile "gamelog.txt"
+./Hurtworld.x86_64 -batchmode -nographics -exec "host 12871;queryport 12881;servername <color=#ff0000>[PERU]</color><color=#ffffff>HURT</color><color=#ff0000>[LATINO]</color> <color=#ffffff>[X3/TP/KIT/]</color> <color=#f1c40f>Unete a nuestra comunidad!</color>;maxplayers 60;autowipe 1;wipeinterval 1209600" -logfile "gamelog.txt"
