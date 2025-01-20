75dcf975dead0f3486529deee3025e485bced3af
Jorge Cayetano <cayetano.laboral@gmail.com>
2025-01-20 15:00:41 -0500

    fix hurtworld server name v4

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,4 +4,4 @@
 cd /home/steam/hurtworld
 
 # Start the Hurtworld server
-./Hurtworld.x86_64 -batchmode -nographics -exec "host 12871;queryport 12881;servername <color=#ff0000> [PERU] </color> HURT <color=#ff0000> [LATINO] </ color> [X3/TP/KIT] Unete a nuestra comunidad!;maxplayers 60;autowipe 1;wipeinterval 1209600" -logfile "gamelog.txt"
+./Hurtworld.x86_64 -batchmode -nographics -exec "host 12871;queryport 12881;servername <color=#ff0000> [PERU] </color> HURT <color=#ff0000> [LATINO] </ color> [X3/TP/KIT] | Unete a nuestra comunidad!;maxplayers 60;autowipe 1;wipeinterval 1209600" -logfile "gamelog.txt"
